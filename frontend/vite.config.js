09288000af474ff178fb5b813dc5996f5fbf5cb2
tech <guytech2003@gmail.com>
2026-05-12 14:03:59 +0530

    ,,

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     target: 'ES2020',
   },
 });
